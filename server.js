c86c9eb36d380a270b989e701eae59d3e1ba4588
karissalbert <karissalbert@gmail.com>
2026-05-28 13:12:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import pg from 'pg';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
